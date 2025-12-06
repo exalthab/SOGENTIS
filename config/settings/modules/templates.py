55200745af8205e4b961febcf15081e2658b1373
exalthab <exalthab@gmail.com>
2025-12-06 11:49:41 +0000

    Sort conflicts-APP_DIRS_True to False/using settings loaders

--- config/settings/modules/templates.py
+++ config/settings/modules/templates.py
@@ -11,7 +11,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
+        "APP_DIRS": False,  # Doit être False si on utilise des loaders personnalisés
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
